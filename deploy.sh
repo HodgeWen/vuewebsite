be7b43c4e0dd40fd4cf96e8a21b6201ed57b755f
wen <emailwen@qq.com>
2018-10-25 13:06:41 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果使用 travis 持续集成
+git push -f https://${access_token}@github.com/wenhongjie/wenhongjie.github.io.git master
+
+cd -
