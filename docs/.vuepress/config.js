39cf4ae981cf192ca09b843148c2324dc0c6290e
wen <emailwen@qq.com>
2018-10-26 17:42:28 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // head左侧标题
-  title: 'WHJ前端知识库',
+  title: 'Web开发',
   description: '每天学到一点',
 
   markdown: {
@@ -15,16 +15,35 @@ module.exports = {
   ],
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Code Guide', link: '/guide/' },
+      { text: 'Learn', 
+        items: [
+          { text: '前端优化', link: '/optimize/' }
+        ]
+      },
+      { text: 'Code Guide', link: '/codeguide/' },
       { text: 'About', link: '/about/' },
+      { text: 'Resource', link: '/resource/' },
       { text: 'GitHub', link: 'https://github.com/wenhongjie' },
     ],
-
-    sidebar: [
-      '/',
-      '/list1/'
-    ],
+    sidebarDepth: 2,
+    sidebar: {
+      '/learn/': [
+        '',
+        'hi'
+      ],
+      '/codeguide/': [
+        ''
+      ],
+      '/about/': [
+        ''
+      ],
+      '/': [
+        '',
+        'learn',
+        'codeguide',
+        'about'
+      ],
+    },
 
     lastUpdated: '最后更新于',
   }
--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,10 @@
-# web前端博客
-website
+---
+home: true
+actionText: 立即进入 →
+actionLink: /learn/
+features:
+- title: 学习 
+  details: 在开发中能学到什么，在项目优化中能学到什么，在生活中能学到什么
+- title: 分享
+  details: 分享是使自己知识体系更加巩固和系统的最佳途径
+---
